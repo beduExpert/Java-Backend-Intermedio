fb327c0a1c490c28854aa4db2183ec65a27129a0
Alex Montoya <amonmar2000@hotmail.com>
2019-12-10 00:10:45 -0600

    Se agrega el código del ejmplo 2

---
+++ Sesion-07/Ejemplo-02/ejemplo2/src/main/java/org/bedu/ejemplo02/Ejemplo02Application.java
@@ -0,0 +1,13 @@
+package org.bedu.ejemplo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo02Application.class, args);
+	}
+
+}

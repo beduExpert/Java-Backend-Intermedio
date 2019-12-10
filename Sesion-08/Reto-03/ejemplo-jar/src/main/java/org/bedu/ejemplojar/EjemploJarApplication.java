d92bf4cf435eefeb339e79f813f9226fad9ddc7b
Alex Montoya <amonmar2000@hotmail.com>
2019-12-10 17:21:30 -0600

    Se agrega el codigo del reto 3

---
+++ Sesion-08/Reto-03/ejemplo-jar/src/main/java/org/bedu/ejemplojar/EjemploJarApplication.java
@@ -0,0 +1,13 @@
+package org.bedu.ejemplojar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploJarApplication.class, args);
+	}
+
+}
---
+++ Sesion-08/Reto-03/ejemplo-jar/src/test/java/org/bedu/ejemplojar/EjemploJarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.ejemplojar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

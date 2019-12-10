809e1768270c7a77e1bc220171f4ca2557542f7c
Alex Montoya <amonmar2000@hotmail.com>
2019-12-10 00:09:02 -0600

    Se agrega el código del ejmplo 1

---
+++ Sesion-07/Ejemplo-01/ejemplo1/src/main/java/org/bedu/ejemplo01/controllers/HelloController.java
@@ -0,0 +1,22 @@
+package org.bedu.ejemplo01.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@RestController
+public class HelloController {
+
+	@GetMapping(value = "hello-world")
+	public Message sayHelloWorld() {
+		return new Message("Hola mundo!!!");
+	}
+
+	@Data
+	@AllArgsConstructor
+	private class Message {
+		private String message;
+	}
+}

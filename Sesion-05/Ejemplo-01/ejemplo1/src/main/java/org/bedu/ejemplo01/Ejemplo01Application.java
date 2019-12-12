08627266e9047b858820720e9a5060ec805424f8
Alex Montoya <amonmar2000@hotmail.com>
2019-12-11 23:22:31 -0600

    Se agregan las imágenes y código del ejemplo 1

---
+++ Sesion-05/Ejemplo-01/ejemplo1/src/main/java/org/bedu/ejemplo01/Ejemplo01Application.java
@@ -0,0 +1,13 @@
+package org.bedu.ejemplo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo01Application.class, args);
+	}
+
+}
---
+++ Sesion-05/Ejemplo-01/ejemplo1/src/test/java/org/bedu/ejemplo01/Ejemplo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.ejemplo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
